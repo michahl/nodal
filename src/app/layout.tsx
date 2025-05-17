805008406f5ca67aeb6cf50a2bbdecb39e1fa209
Michail Christoforatos <mchristoforatos@icloud.com>
2025-05-17 13:54:08 +0300

    refactor layout component for improved structure and styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="relative mx-auto h-screen w-full max-w-6xl px-6 md:px-8 lg:px-12">
+    <div className="relative mx-auto flex h-dvh w-full max-w-6xl flex-col px-6 md:px-8 lg:px-12">
       <header className="flex items-center justify-between py-8">
         <Link href="/">
           <span className="text-neutral-950">gnosei</span>
@@ -24,16 +24,16 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
           </Link>
         </nav>
       </header>
-      <main>{children}</main>
+      <main className="flex-1">{children}</main>
       <footer>
-       <div className='flex items-center justify-center py-8'>
-          <span className='text-sm text-neutral-800'>
+        <div className="flex items-center justify-center py-8">
+          <span className="text-sm text-neutral-800">
             Made by
             <a
-              href='https://github.com/michahl'
-              target='_blank'
-              rel='noopener noreferrer'
-              className='ml-1 rounded-full bg-neutral-100 px-2 py-1 text-neutral-950'
+              href="https://github.com/michahl"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="ml-1 rounded-full bg-neutral-100 px-2 py-1 text-neutral-950"
             >
               @michahl
             </a>
@@ -41,8 +41,8 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         </div>
       </footer>
     </div>
-  )
-}
+  );
+};
 
 export default function RootLayout({
   children,
